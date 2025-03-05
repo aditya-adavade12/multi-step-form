edf94863c91de18deff1916af4b27fe6beb151eb
Aditya Adavade <adityawork118@gmail.com>
2025-03-05 08:31:47 +0530

    New updates in code

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -9,8 +9,57 @@ const Form = () => {
   const next = useRef();
   const submit = useRef();
   const btns = useRef();
+  const handleError = useRef();
   // Step Counter 
   const [Step, setStep] = useState(1);
+  // Next Step Function
+  const nextStep = (count) => {
+    switch (count) {
+      case 1:
+        step1.current.style.display = "none";
+        step2.current.style.display = "block";
+        setStep(Step + 1);
+        break;
+      case 2:
+        step2.current.style.display = "none";
+        step3.current.style.display = "block";
+        next.current.style.display = "none";
+        submit.current.style.display = "block";
+        setStep(3);
+        break;
+      default:
+        console.log("Error");
+        break;
+    }
+  }
+  // Previous Step Function
+  const prevStep = (count) => {
+    if (count === 3) {
+      step3.current.style.display = "none";
+      step2.current.style.display = "block";
+      next.current.style.display = "block";
+      submit.current.style.display = "none";
+      setStep(Step - 1);
+    } else if (count === 2) {
+      step2.current.style.display = "none";
+      step1.current.style.display = "block";
+      setStep(Step - 1);
+    } else if (count === 1) {
+      step1.current.style.display = "block";
+      step2.current.style.display = "none";
+      setStep(1);
+    }
+  }
+  // Form 
+  const [Form, setForm] = useState({name: "", gender: "", dob: "", phone: "", email: "", username: "", password: ""})
+  // Input Handler
+  const inputHandler = (e) => {
+    let {name, value} = e.target;
+    setForm((prevValue) => ({
+      ...prevValue, 
+      [name] : value,
+    }));
+  }
   return (
     <div className="text-white">
       {/* Progress Bar */}
@@ -30,8 +79,11 @@ const Form = () => {
       {/* Form Container */}
       <div
         id="form-container"
-        className="p-4 w-[50vw] mx-auto mt-8 rounded-lg border border-slate-900 max-sm:w-[90vw]"
+        className="p-4 w-[50vw] mx-auto mt-8 rounded-lg border border-stone-900 max-sm:w-[90vw]"
       >
+        {/* Error */}
+        <div id="error" className="bg-red-800 py-1 mb-1.5 rounded-md font-semibold px-1.5 hidden" ref={handleError}>
+        </div>
         {/* Form 1 */}
         <div ref={step1}>
           <div id="form1" className="flex flex-col justify-start gap-3">
@@ -43,8 +95,10 @@ const Form = () => {
                 type="text"
                 name="name"
                 id="name"
+                value={Form.name}
+                onChange={inputHandler}
                 placeholder="Your first name..."
-                className="w-full border border-gray-800 px-1.5 py-1.5 rounded-lg outline-none"
+                className="w-full border border-stone-800 px-1.5 py-1.5 rounded-lg outline-none"
                 required
               />
             </div>
@@ -54,11 +108,11 @@ const Form = () => {
               </label>
               <div className="flex flex-row items-center gap-2">
                 <div>
-                  <input type="radio" name="gender" id="male" />{" "}
+                  <input type="radio" name="gender" id="male" onChange={inputHandler} value="Male" checked={Form.gender == "Male"} />{" "}
                   <label htmlFor="male">Male</label>
                 </div>
                 <div>
-                  <input type="radio" name="gender" id="female" />{" "}
+                  <input type="radio" name="gender" id="female" onChange={inputHandler} value="Female" checked={Form.gender == "Female"} />{" "}
                   <label htmlFor="female">Female</label>
                 </div>
               </div>
@@ -71,7 +125,9 @@ const Form = () => {
                 type="date"
                 name="dob"
                 id="dob"
-                className="w-full border border-gray-800 px-1.5 py-1.5 rounded-lg outline-none"
+                value={Form.dob}
+                onChange={inputHandler}
+                className="w-full border border-stone-800 px-1.5 py-1.5 rounded-lg outline-none"
                 required
               />
             </div>
@@ -88,8 +144,10 @@ const Form = () => {
                 type="text"
                 name="phone"
                 id="phone"
+                value={Form.phone}
+                onChange={inputHandler}
                 placeholder="Your phone number..."
-                className="w-full border border-gray-800 px-1.5 py-1.5 rounded-lg outline-none"
+                className="w-full border border-stone-800 px-1.5 py-1.5 rounded-lg outline-none"
                 required
               />
             </div>
@@ -101,8 +159,10 @@ const Form = () => {
                 type="email"
                 name="email"
                 id="email"
+                value={Form.email}
+                onChange={inputHandler}
                 placeholder="Your email address..."
-                className="w-full border border-gray-800 px-1.5 py-1.5 rounded-lg outline-none"
+                className="w-full border border-stone-800 px-1.5 py-1.5 rounded-lg outline-none"
                 required
               />
             </div>
@@ -119,8 +179,10 @@ const Form = () => {
                 type="text"
                 name="username"
                 id="username"
+                value={Form.username}
+                onChange={inputHandler}
                 placeholder="Your username..."
-                className="w-full border border-gray-800 px-1.5 py-1.5 rounded-lg outline-none"
+                className="w-full border border-stone-800 px-1.5 py-1.5 rounded-lg outline-none"
                 required
               />
             </div>
@@ -132,8 +194,10 @@ const Form = () => {
                 type="password"
                 name="password"
                 id="password"
+                value={Form.password}
+                onChange={inputHandler}
                 placeholder="Your password..."
-                className="w-full border border-gray-800 px-1.5 py-1.5 rounded-lg outline-none"
+                className="w-full border border-stone-800 px-1.5 py-1.5 rounded-lg outline-none"
                 required
               />
             </div>
@@ -147,18 +211,21 @@ const Form = () => {
         ref={btns}
       >
         <button
-          className="bg-gray-900 w-full p-1.5 rounded-lg cursor-pointer hover:opacity-85 transition-all outline-none"
+          className="bg-stone-950 w-full p-1.5 rounded-lg cursor-pointer hover:opacity-85 transition-all outline-none"
           ref={prev}
+          onClick={() => prevStep(Step)}
         >
           Prev
         </button>
         <button
           className="bg-blue-500 w-full p-1.5 rounded-lg cursor-pointer hover:opacity-85 transition-all outline-none"
           ref={next}
+          onClick={() => nextStep(Step)}
         >
           Next
         </button>
-        <button className="bg-blue-500 w-full p-1.5 rounded-lg cursor-pointer hover:opacity-85 transition-all outline-none hidden" ref={submit}>
+        <button className="bg-white text-black w-full p-1.5 rounded-lg cursor-pointer hover:opacity-85 transition-all outline-none hidden" ref={submit} onClick={() => console.log(Form)
+        }>
           Submit
         </button>
       </div>
